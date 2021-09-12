9d83733e374717ac17e8b032dad70eeb402ae274
saico09 <marco.contreras@alumnos.upla.cl>
2021-09-12 20:11:51 -0300

    arreglo ingresar alumno

--- SkeletonAPP/src/app/home-alumno/home-alumno.page.ts
+++ SkeletonAPP/src/app/home-alumno/home-alumno.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home-alumno',
@@ -7,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeAlumnoPage implements OnInit {
   img:String;
-  constructor() { }
+  dato:String;
+  constructor(public toastController: ToastController,private router:Router) {}
 
   ngOnInit() {
     this.img ='./assets/img/1.png';
+  }
+  bienvenida(){
+    //Llamar a Toast
+    this.presentToast("Bienvenido "+this.dato)
+  }
+  ingresa(){
+
+    let navigationExtra :NavigationExtras={
+      state:{dato: this.dato}
+    };
+    //Utilizar API enrutador para llamar a la siguiente página
+
+    this.router.navigate(['/pagealumno'],navigationExtra);
+
+  }
+  async presentToast(msg:string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
