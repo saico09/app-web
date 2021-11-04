f4c3a876cac851152c956eb276b13555343b7c89
jav123123 <javie.molinac@alumnos.duoc.cl>
2021-11-04 17:51:55 -0300

    commit

--- SkeletonAPP/src/app/pages/not-found/not-found.page.ts
+++ SkeletonAPP/src/app/pages/not-found/not-found.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) {}
 
   ngOnInit() {
+  }
+  ingresa3(){
+
+    //Utilizar API enrutador para llamar a la siguiente página
+
+    this.router.navigate(['/inicio']);
+
   }
 
 }
